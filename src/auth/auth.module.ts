1b49b1d5fa71b89c6dc88865590b835b8fa2b504
Gusth Gomes <gusthgomesdarosa@gmail.com>
2024-08-15 21:37:52 -0300

    chore: using user and prisma module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { AuthController } from './auth.controler';
+import { UserModule } from 'src/user/user.module';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
   imports: [
     JwtModule.register({
       secret: '|8*`Y28]k079(F~B^3l.(D`O>1()Zxok',
     }),
+    UserModule,
+    PrismaModule,
   ],
+  controllers: [AuthController],
 })
 export class AuthModule {}
