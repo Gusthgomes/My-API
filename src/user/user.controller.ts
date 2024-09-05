a51c47562e04b3c1d0baef7d11a397cacd2fc73d
Gusth Gomes <gusthgomesdarosa@gmail.com>
2024-09-04 21:36:13 -0300

    feat: add roleGuard in all controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -20,43 +20,38 @@ import { Role } from 'src/enums/role.enums';
 import { RoleGuard } from 'src/guards/Role.Guard';
 import { AuthGuard } from 'src/guards/Auth.Guard';
 
+@RolesDecorator(Role.Admin)
 @UseGuards(AuthGuard, RoleGuard)
 @UseInterceptors(LogInterceptor)
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @RolesDecorator(Role.Admin)
   @Post()
   async create(@Body() data: CreateUserDTO) {
     return this.userService.create(data);
   }
 
-  @RolesDecorator(Role.Admin)
   @Get()
   async list() {
     return this.userService.list();
   }
 
-  @RolesDecorator(Role.Admin)
   @Get(':id')
   async show(@ParamId() id: number) {
     return this.userService.show(id);
   }
 
-  @RolesDecorator(Role.Admin)
   @Put(':id')
   async update(@Body() data: UpdatePutUserDTO, @ParamId() id: number) {
     return this.userService.update(id, data);
   }
 
-  @RolesDecorator(Role.Admin)
   @Patch(':id')
   async updatePartial(@Body() data: UpdatePatchUserDTO, @ParamId() id: number) {
     return this.userService.updatePartial(id, data);
   }
 
-  @RolesDecorator(Role.Admin)
   @Delete(':id')
   async delete(@ParamId() id: number) {
     return this.userService.delete(id);
