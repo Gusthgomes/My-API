4a0782609e774952062020e66dbe8cc71f6dfe8c
Gusth Gomes <gusthgomesdarosa@gmail.com>
2024-09-04 21:16:57 -0300

    fix: correction in error circular dependencies

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controler';
 import { UserModule } from 'src/user/user.module';
@@ -10,10 +10,11 @@ import { AuthService } from './auth.service';
     JwtModule.register({
       secret: '|8*`Y28]k079(F~B^3l.(D`O>1()Zxok',
     }),
-    UserModule,
+    forwardRef(() => UserModule),
     PrismaModule,
   ],
   controllers: [AuthController],
   providers: [AuthService],
+  exports: [AuthService],
 })
 export class AuthModule {}
